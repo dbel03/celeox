34806c5e3e4f7dcac6fc91ea19046e2659eda6e6
3davidbel3@gmail.com <your.email@example.com>
2026-08-23 20:21:49 +0200

    fix

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -5,7 +5,7 @@ import MapView from '../components/map/MapView'
 function MapPage() {
 
     return (
-        <main className="relative h-screen w-screen overflow-hidden">
+        <main className="relative h-dvh w-full overflow-hidden">
 
             <AppNavbar floating />
 
